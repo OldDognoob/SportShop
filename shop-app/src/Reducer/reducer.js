1042174014bc0b5c29ebf841c037eec1e572cb00
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-18 12:03:45 +0300

    wip-reducer

--- shop-app/src/Reducer/reducer.js
+++ shop-app/src/Reducer/reducer.js
@@ -15,3 +15,47 @@ const initialStore = {
   total: 0,
   amount: 0,
 };
+function reducer(state = initialStore, action) {
+  if (action.type === CLEAR_CART) {
+    return {
+      ...state,
+      cart: [],
+    };
+  }
+  if (action.type === DECREASE) {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = {
+          ...cartItem,
+          amount: cartItem.amount - 1,
+        };
+      }
+      return cartItem;
+    });
+    return {
+      ...state,
+      cart: tempCart,
+    };
+  }
+  if (action.type === INCREASE) {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = {
+          ...cartItem,
+          amount: cartItem.amount + 1,
+        };
+      }
+      return cartItem;
+    });
+    return {
+      ...state,
+      cart: tempCart,
+    };
+  }
+  if (action.type === REMOVE) {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload.id),
+    };
+  }
+}
