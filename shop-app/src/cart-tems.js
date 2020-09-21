2f787c76b61fe6934c144dd8744e5634ac403a90
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-21 11:10:04 +0300

    cart-items

---
+++ shop-app/src/cart-tems.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: 1,
+    title: "Michael Jordan 23",
+    price: 1.5,
+    img: "../src/images/MJ23Purple.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "MJ23 OldSchool",
+    price: 2.999,
+    img: "../src/images/MJOldSchool.jpg",
+    amount: 2,
+  },
+  {
+    id: 3,
+    title: "MJ23",
+    price: 1.7,
+    img: "../src/images/MJ23White.jpg",
+    amount: 3,
+  },
+  {
+    id: 4,
+    title: "MJ23",
+    price:500.00,
+    img:"../src/images/MJ23WhiteRed.jpg" ,
+    amount:4 
+  }
+  {
+      id:5,
+      title: "MJ23",
+      price: 750.00,
+      img:"../src/images/MJ23Purple.jpg",
+      amount:5
+  },
+  {
+      id:6,
+      title:"NIKE",
+      price: 200.00,
+      img:"../src/images/NIKEAIRRedFire.jpg",
+      amount:6
+  },
+  {
+      id: 7,
+      title:"NIKE",
+      price: 150.00,
+      img: "../src/images/NIKEBlack&White.jpg",
+      amount:7
+  },
+  {
+      id:8,
+      title:"NIKE",
+      price:120.00,
+      img: "../src/images/nike.jpg",
+      amount:8
+  }
+];
